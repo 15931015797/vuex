19bd8a46cccbd97bc627d2cded36f6248062ed2b
zhuzhu <z15931015797@163.com>
2019-07-09 17:19:41 +0800

    配置路由引入插件

---
+++ src/views/home/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>home组件</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>login组件</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
