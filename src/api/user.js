1e6ed320ac6862c1934e3393fcd251a3fff0eb03
zhuzhu <z15931015797@163.com>
2019-07-09 19:10:02 +0800

    封装api请求函数

---
+++ src/api/user.js
@@ -0,0 +1,12 @@
+// 用户相关模块
+import request from '@/utils/request'
+export const login = ({ mobile, code }) => {
+  return request({
+    method: 'POST',
+    url: '/app/v1_0/authorizations',
+    data: {
+      mobile,
+      code
+    }
+  })
+}
